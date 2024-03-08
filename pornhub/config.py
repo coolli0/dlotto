eedfc38c7e82f63ac24efc0463091900a7323208
coolli0 <162727852+coolli0@users.noreply.github.com>
2024-03-09 02:53:51 +0300

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -1,10 +1,10 @@
 from typing import List
 
-API_ID: int = 12345
-API_HASH: str = "abcdefg:12345678"
-TOKEN: str = "abcdefg:12345678"
+API_ID: int = 25922109
+API_HASH: str = "3a80ff566d32efce8835df46ed125b53"
+TOKEN: str = "6505708104:AAE2_NmyNPuh3ee1QRlmsbv6OaKhvbMWeWE"
 
-log_chat: int = -12345678
-sub_chat: str = "channel_username"
-sudoers: List[int] = [1757169682, 859229457]
+log_chat: int = -1004122520162
+sub_chat: str = "BigAssGirlz"
+sudoers: List[int] = [1108915205, 85922945]
 prefixs: List[str] = ["/", "!", ".", "$"]
