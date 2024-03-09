5322c7e59cd0ef9dbbe025c6c5f97cd542112e09
coolli0 <162727852+coolli0@users.noreply.github.com>
2024-03-09 03:01:56 +0300

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -4,7 +4,7 @@ API_ID: int = 25922109
 API_HASH: str = "3a80ff566d32efce8835df46ed125b53"
 TOKEN: str = "6505708104:AAE2_NmyNPuh3ee1QRlmsbv6OaKhvbMWeWE"
 
-log_chat: int = -1004122520162
+log_chat: int = -4122520162
 sub_chat: str = "BigAssGirlz"
 sudoers: List[int] = [1108915205, 85922945]
 prefixs: List[str] = ["/", "!", ".", "$"]
